318653945f4ff1fe65d9a425b66f14f8eff87da1
Shekhar Kumar <shekhar.mnnit10@gmail.com>
2016-10-23 01:46:48 +0530

    Indentation to spaces

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ import App from './App';
 import Home from './containers/Home';
 
 export default () => {
-	/*
-	* Please keep routes in alphabetical order
-	*/
-	return (
-		<Route path="/" component = { App }>
+    /*
+    * Please keep routes in alphabetical order
+    */
+    return (
+        <Route path="/" component = { App }>
             <Route path = "home" component = { Home } />
-		</Route>
-	);
+        </Route>
+    );
 }
